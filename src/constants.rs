0f7d2960c0adae8deebd127709b8f7f8382d7f92
Andrey <otbacmhe@gmail.com>
2020-02-13 23:51:13 +0300

    add some intaractivity

--- src/constants.rs
+++ src/constants.rs
@@ -18,8 +18,8 @@ pub const WINDOW_HEIGHT: u32 = 1024;
 pub const FRACTAL_CENTER_X: f64 = -0.37; // X center position
 pub const FRACTAL_CENTER_Y: f64 = 0.65; // Y center position
 
-pub const ZOOM: f64 = 0.01; // Y center position
-pub const LIMIT: f64 = 500.0; // Y center position
+pub const ZOOM: f64 = 0.1; // Y center position
+pub const LIMIT: f64 = 150.0; // Y center position
 
 pub const FRACTAL_SCALE: f32 = 0.1;
 
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use ggez::event;
 use ggez::graphics;
 use ggez::timer;
 use ggez::nalgebra as na;
-use ggez::{conf::*, Context, GameResult, mint,  graphics::*};
+use ggez::{conf::*, Context, GameResult, mint,  graphics::*, event::*};
 use constants::*;
 use rayon::prelude::*;
 use num::Complex;
@@ -76,91 +76,18 @@ fn get_splines() -> Splines {
 }
 
 
-
-// fn get_color(count: &Option<usize>) -> Vec<u8> {
-//    let palette1 = vec![
-//        vec![0x18, 0x4d, 0x68, 255],
-//        vec![0x31, 0x80, 0x9f, 255],
-//        vec![0xfb, 0x9c, 0x6c, 255],
-//        vec![0xd5, 0x51, 0x21, 255],
-//        vec![0xcf, 0xe9, 0x90, 255],
-//        vec![0xea, 0xfb, 0xc5, 255]
-//    ];
-//    let palette2 = vec![
-//        vec![0x00, 0x00, 0x00, 255],
-//        vec![0x03, 0x26, 0x28, 255],
-//        vec![0x07, 0x3e, 0x1e, 255],
-//        vec![0x18, 0x55, 0x08, 255],
-//        vec![0x5f, 0x6e, 0x0f, 255],
-//        vec![0x84, 0x50, 0x19, 255],
-//        vec![0x9b, 0x30, 0x22, 255],
-//        vec![0xb4, 0x92, 0x2f, 255],
-//        vec![0x94, 0xca, 0x3d, 255],
-//        vec![0x4f, 0xd5, 0x51, 255],
-//        vec![0x66, 0xff, 0xb3, 255],
-//        vec![0x82, 0xc9, 0xe5, 255],
-//        vec![0x9d, 0xa3, 0xeb, 255],
-//        vec![0xd7, 0xb5, 0xf3, 255],
-//        vec![0xfd, 0xd6, 0xf6, 255],
-//        vec![0xff, 0xf0, 0xf2, 255],
-//    ];
-//    let mut x : [f64; 16] = [0.0; 16];
-//    let mut r : [f64; 16] = [0f64; 16];
-//    let mut g : [f64; 16] = [0f64; 16];
-//    let mut b : [f64; 16] = [0f64; 16];
-//
-//    for i in 0usize..16usize {
-//        x[i] = i as f64/15.0;
-//        r[i] = palette2[i][0] as f64;
-//        g[i] = palette2[i][1] as f64;
-//        b[i] = palette2[i][2] as f64;
-//    }
-//
-//    let mut acc_r = rgsl::InterpAccel::new();
-//    let mut acc_g = rgsl::InterpAccel::new();
-//    let mut acc_b = rgsl::InterpAccel::new();
-//    let spline_r = rgsl::Spline::new(&rgsl::InterpType::cspline(), 16).unwrap();
-//    let spline_g = rgsl::Spline::new(&rgsl::InterpType::cspline(), 16).unwrap();
-//    let spline_b = rgsl::Spline::new(&rgsl::InterpType::cspline(), 16).unwrap();
-//
-//    spline_r.init(&x, &r);
-//    spline_g.init(&x, &g);
-//    spline_b.init(&x, &b);
-//    let spline_r = spline[0];
-//    let spline_g = spline[1];
-//    let spline_b = spline[2];
-//
-//    let acc_r =
-
-//    match count {
-//        None => vec![
-//            splines.r.eval(1, &mut splines.ra) as u8,
-//            spline.g.eval(1, &mut splines.ga) as u8,
-//            spline.b.eval(1, &mut splines.gb) as u8,
-//            255
-//        ].clone(),
-//        Some(count) => {
-//            let xi = 1 as f64 - (*count as f64/LIMIT as f64);
-//            vec![
-//                spline_r.eval(xi, &mut acc_r) as u8,
-//                spline_g.eval(xi, &mut acc_g) as u8,
-//                spline_b.eval(xi, &mut acc_b) as u8,
-//                255
-//            ].clone()
-//        }
-//    }
-// }
-
 struct MainState {
     fractal_buffer: Vec<u8>,
     fractal_rendered: bool,
-    splines: Splines
+    splines: Splines,
+    zoom: f64,
+    limit: f64,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
         let initial_buffer = Vec::with_capacity((WINDOW_WIDTH as usize * WINDOW_HEIGHT as usize * 4) as usize);
-        let s = MainState { fractal_buffer: initial_buffer, fractal_rendered: false, splines:  get_splines()};
+        let s = MainState { fractal_buffer: initial_buffer, fractal_rendered: false, splines:  get_splines(), zoom: ZOOM, limit: LIMIT};
         Ok(s)
     }
     fn get_color(&mut self, count: &Option<usize>) -> Vec<u8>  {
@@ -172,7 +99,7 @@ impl MainState {
                 255
             ].clone(),
             Some(count) => {
-                let xi = 1 as f64 - (*count as f64/LIMIT as f64);
+                let xi = 1 as f64 - (*count as f64/self.limit as f64);
                 vec![
                     self.splines.r.eval(xi, &mut self.splines.ra) as u8,
                     self.splines.g.eval(xi,  &mut self.splines.ga) as u8,
@@ -194,14 +121,14 @@ impl event::EventHandler for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
         let ratio = WINDOW_WIDTH as f64 / WINDOW_HEIGHT as f64;
-        let zoom = ZOOM;
+        let zoom = self.zoom;
         let center_x = FRACTAL_CENTER_X;
         let center_y = 0.0- FRACTAL_CENTER_Y;
         let min_x = center_x - (zoom / 2.0);
         let min_y = center_y - (zoom / 2.0 / ratio);
-        let iterations = LIMIT;
+        let iterations = self.limit;
         if !self.fractal_rendered {
             let colors = (0..(WINDOW_WIDTH  * WINDOW_HEIGHT) as usize)
                 .into_par_iter()
@@ -254,6 +181,24 @@ impl event::EventHandler for MainState {
 
         graphics::present(ctx)?;
         Ok(())
+    }
+    fn key_up_event(&mut self, ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods) {
+        if keycode == KeyCode::Z {
+            self.zoom += 0.05 * self.zoom;
+            self.fractal_rendered = false;
+        }
+        if keycode == KeyCode::X {
+            self.zoom -= 0.05 * self.zoom;
+            self.fractal_rendered = false;
+        }
+        if keycode == KeyCode::C {
+            self.limit += 0.5 * self.limit;
+            self.fractal_rendered = false;
+        }
+        if keycode == KeyCode::V {
+            self.limit -= 0.5 * self.limit;
+            self.fractal_rendered = false;
+        }
     }
 }
 
